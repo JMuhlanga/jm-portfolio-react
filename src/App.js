97748ec9bb1b4ae2c7fd28a4127285030f3d9bbe
Jose Kupeka Muhlanga <muhlangakupeka@outlook.com>
2023-08-09 19:48:44 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import NavBar from './Components/NavBar';
+import About from './Components/About';
+import Skills from './Components/Skills';
+import Hero from './Components/Hero';
+import Projects from './Components/Projects';
+import Testimonials from './Components/Testimonials';
+import Contact from './Components/Contact';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      
     </div>
   );
 }

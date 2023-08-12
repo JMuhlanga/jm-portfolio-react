c58702b8a4b591ba754db62ee695da9eed2aec2e
Jose Kupeka Muhlanga <muhlangakupeka@outlook.com>
2023-08-12 15:53:20 +0300

    Addition of Contact details

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div class="contact-section">
+      <div class="mycontacts">
+      
+        <p><b>Email: </b>muhlangakupeka@outlook.com</p>
+        <p><b>Phone1: </b>+254735219317</p>
+        <p><b>Phone2: </b>+254113771641</p>
+        <p><b>LinkedIn: </b>www.linkedin.com/in/josekmuhlanga</p>
+        <p><b>Github: </b>www.github.com/JMuhlanga</p>
+        <p><b>Twitter: </b>www.twitter.com/MuhlangaJose</p>
+        <p><b>Instagram: </b>https://www.instagram.com/jnr_muhlanga/</p>
+
+      </div>
+
+    </div>
+    
   )
 }
 

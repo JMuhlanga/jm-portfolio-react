be345aef701854ea15df097beff44e57821dfdcb
Jose Kupeka Muhlanga <muhlangakupeka@outlook.com>
2023-08-12 19:48:04 +0300

    Addition of Skills Icons

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -4,9 +4,51 @@ function Skills() {
   return (
     <div>
       <h3>Skills</h3>
-      <p><b>Programming Languages:</b> HTML, CSS, JavaScript, Java, PHP, Ruby, SQL, Bash Scripting</p>
-      <p><b>Frameworks:</b> React.js, Next.js, Ruby on Rails, Laravel, Spring Boot, Tailwind CSS, NodeJS</p>
-      <p><b>CI/CD , Version Control and other tools: </b> Git, Docker, Linux, Postman</p>
+
+        <div className="skill-mini-divs">
+          <h5>Programming Languages</h5>
+          <p>HTML, CSS, JavaScript, Java, PHP, Ruby,Bash Scripting</p>
+
+          <div className="skills-icons">
+            <i class="fa-brands fa-html5"></i>
+            <i class="fa-brands fa-css3-alt"></i>
+            <i class="fa-brands fa-square-js"></i>
+            <i class="fa-brands fa-java"></i>
+            <i class="fa-brands fa-php"></i>
+            <i class="fa-solid fa-gem"></i>
+            <i class="fa-solid fa-terminal"></i>
+
+          </div>
+          
+        </div>
+
+        <div className="skill-mini-divs">
+          <h5>Frameworks</h5>
+          <p>React.js, Next.js, Ruby on Rails, Laravel, Spring Boot, Tailwind CSS, NodeJS</p>
+          
+          <div className="skills-icons">
+            <i class="fa-brands fa-react"></i>
+            <i class="fa-solid fa-n"></i>
+            <i class="fa-brands fa-laravel"></i>
+            <i class="fa-brands fa-node"></i>
+            <i class="fa-solid fa-leaf"></i>
+          </div>
+        </div>
+
+        <div className="skill-mini-divs">
+          <h5>CI/CD , Version Control and other tools</h5>
+          <p>Git, Docker, Linux, Postman</p>
+
+          <div className="skills-icons">
+            <i class="fa-brands fa-git"></i>
+            <i class="fa-brands fa-github"></i>
+            <i class="fa-brands fa-linux"></i>
+            <i class="fa-brands fa-docker"></i>
+            <i class="fa-solid fa-person"></i>
+
+          </div>
+        </div>
+      
     </div>
   )
 }

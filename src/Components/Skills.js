e479ef516bc8c57a9672dc17e3ad4788100e66fa
Jose Kupeka Muhlanga <muhlangakupeka@outlook.com>
2023-08-12 20:42:51 +0300

    End of Saturday 12/08/2023

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,50 +1,51 @@
 import React from 'react'
+import '../Style/Skills.css';
 
 function Skills() {
   return (
-    <div>
-      <h3>Skills</h3>
+    <div className="skills-container">
+      <h2>Skills</h2>
 
         <div className="skill-mini-divs">
-          <h5>Programming Languages</h5>
-          <p>HTML, CSS, JavaScript, Java, PHP, Ruby,Bash Scripting</p>
+          <h3>Programming Languages:</h3>
+          <p><b>HTML, CSS, JavaScript, Java, PHP, Ruby,Bash Scripting</b></p>
 
           <div className="skills-icons">
-            <i class="fa-brands fa-html5"></i>
-            <i class="fa-brands fa-css3-alt"></i>
-            <i class="fa-brands fa-square-js"></i>
-            <i class="fa-brands fa-java"></i>
-            <i class="fa-brands fa-php"></i>
-            <i class="fa-solid fa-gem"></i>
-            <i class="fa-solid fa-terminal"></i>
+            <i class="icon fa-brands fa-html5"></i>
+            <i class="icon fa-brands fa-css3-alt"></i>
+            <i class="icon fa-brands fa-square-js"></i>
+            <i class="icon fa-brands fa-java"></i>
+            <i class="icon fa-brands fa-php"></i>
+            <i class="icon fa-solid fa-gem"></i>
+            <i class="icon fa-solid fa-terminal"></i>
 
           </div>
           
         </div>
 
         <div className="skill-mini-divs">
-          <h5>Frameworks</h5>
-          <p>React.js, Next.js, Ruby on Rails, Laravel, Spring Boot, Tailwind CSS, NodeJS</p>
+          <h3>Frameworks:</h3>
+          <p><b>React.js, Next.js, Ruby on Rails, Laravel, Spring Boot, Tailwind CSS, NodeJS</b></p>
           
           <div className="skills-icons">
-            <i class="fa-brands fa-react"></i>
-            <i class="fa-solid fa-n"></i>
-            <i class="fa-brands fa-laravel"></i>
-            <i class="fa-brands fa-node"></i>
-            <i class="fa-solid fa-leaf"></i>
+            <i class="icon fa-brands fa-react"></i>
+            <i class="icon fa-solid fa-n"></i>
+            <i class="icon fa-brands fa-laravel"></i>
+            <i class="icon fa-brands fa-node"></i>
+            <i class="icon fa-solid fa-leaf"></i>
           </div>
         </div>
 
         <div className="skill-mini-divs">
-          <h5>CI/CD , Version Control and other tools</h5>
-          <p>Git, Docker, Linux, Postman</p>
+          <h3>CI/CD , Version Control and other tools:</h3>
+          <p><b>Git, Docker, Linux, Postman</b></p>
 
           <div className="skills-icons">
-            <i class="fa-brands fa-git"></i>
-            <i class="fa-brands fa-github"></i>
-            <i class="fa-brands fa-linux"></i>
-            <i class="fa-brands fa-docker"></i>
-            <i class="fa-solid fa-person"></i>
+            <i class="icon fa-brands fa-git"></i>
+            <i class="icon fa-brands fa-github"></i>
+            <i class="icon fa-brands fa-linux"></i>
+            <i class="icon fa-brands fa-docker"></i>
+            <i class="icon fa-solid fa-person"></i>
 
           </div>
         </div>

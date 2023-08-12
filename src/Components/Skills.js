eaeeba42fa89c24bf31da9df824ab112a5377d96
Jose Kupeka Muhlanga <muhlangakupeka@outlook.com>
2023-08-12 15:42:31 +0300

    Addition of Skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div>
+      <h3>Skills</h3>
+      <p><b>Programming Languages:</b> HTML, CSS, JavaScript, Java, PHP, Ruby, SQL, Bash Scripting</p>
+      <p><b>Frameworks:</b> React.js, Next.js, Ruby on Rails, Laravel, Spring Boot, Tailwind CSS, NodeJS</p>
+      <p><b>CI/CD , Version Control and other tools: </b> Git, Docker, Linux, Postman</p>
+    </div>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,3 @@
+// Below are the projects that I have done or been involved in 
+
+export const projects = [];
